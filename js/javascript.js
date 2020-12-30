d530549da296ac60cbcb2042c34b45b429eb3c75
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2020-12-30 20:45:30 -0300

    atualizacao de interface e melhoria na versao mobile

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,13 @@
 const close = document.getElementById("close");
 const header =  document.querySelector("header");
+const button_open = document.getElementById("button-open")
+console.log(button_open)
 
 
 close.addEventListener('click', function() {
    header.classList.remove('header-open')
 })
 
+button_open.addEventListener('click', ()=>{
+   header.classList.add('header-open')
+})
