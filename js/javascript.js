ff6c90d5dd1701222b8d9335118d522ebde79473
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2021-01-05 20:50:24 -0300

    atualizacao de interface

--- js/javascript.js
+++ js/javascript.js
@@ -5,6 +5,11 @@ const button_open_nav = document.getElementById("button-open-nav");
 const nav = document.querySelector("nav")
 const nav_close = document.getElementById("close-mobile");
 
+const certificadoOpen = document.getElementById("button-open-certificado");
+const container = document.querySelector("container")
+const  cursosClose = document.getElementById("cursos-close-button")
+console.log(container)
+
 close.addEventListener('click', ()=> {
    header.classList.remove('header-open')
 })
@@ -19,4 +24,12 @@ button_open_nav.addEventListener('click', ()=>{
 
 nav_close.addEventListener('click', function() {  
    nav.classList.remove('nav-open')
+})
+
+certificadoOpen.addEventListener('click', function() {
+   container.classList.add('cursos')
+})
+
+cursosClose.addEventListener('click', ()=>{
+   container.classList.remove('cursos')
 })
