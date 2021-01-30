99262af82c2b66cb0849ebefce5250d28f8edbc2
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2021-01-30 18:19:00 -0300

    projeto finalizado

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+document.getElementById('mobile-open').onclick = ()=>{
+    document.getElementById('menu-header').classList.remove('open')
+}
+
+document.getElementById('close').onclick = ()=>{
+    document.getElementById('menu-header').classList.add('open')
+}
+
+document.getElementById('projeto-open').onclick = ()=>{
+    document.getElementById('projetos').classList.remove('projetos-open')
+}
+
+document.getElementById('projetos-close').onclick = ()=>{
+   document.getElementById("projetos").classList.add('projetos-open')
+}
+  
